c99d8c349e6d035fc89af8d4a29af43ab7a7aa9f
Shivam Gupta <shiva6588@gmail.com>
2021-06-29 10:09:52 +0530

    marketing panel

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+// function load_sms(){
+//   $(document).ready(function(){
+//       $(".change-section").load("Sms-panel.html");});
+// };
+// function load_mail(){
+//     $(document).ready(function(){
+//         $(".change-section").load("Email-panel.html");});
+//   };
+
+  
